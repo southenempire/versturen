5fed9843183b887f7f93d635ac56928a63719630
Anthonyushie <anthonytwan75official@gmail.com>
2024-08-25 11:44:57 +0100

    initial commit✅

---
+++ client/Versturen/src/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App';
+
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <Router>
+        <App />
+      </Router>
+    </React.StrictMode>
+  );
+} else {
+  console.error("Root element with id 'root' not found.");
+}
