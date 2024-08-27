77ea69133ec3c9b30b16d1f2eeea0b491dddd831
Anthonyushie <anthonytwan75official@gmail.com>
2024-08-27 22:01:08 +0100

    Building Sidebar and Navbar ⚙

---
+++ client/Versturen/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{html,js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        epilogue: ['Epilogue', 'sans-serif'],
+      },
+      boxShadow: {
+        secondary: '10px 10px 20px rgba(2, 2, 2, 0.25)',
+      },
+    },
+  },
+  plugins: [],
+}
+
